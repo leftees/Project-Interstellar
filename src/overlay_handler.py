a7dc2601517beeafe830f79294fd2d9b7814fda2
Max Lange <max12354678910@gmail.com>
2016-03-25 19:04:59 +0100

    added overlay handler

--- src/overlay_handler.py
+++ src/overlay_handler.py
@@ -6,55 +6,105 @@ class create_overlay():
 	"""A mother-class for in-game overlays.
 
 	init(self) inits the module
-	add_overlay(self, overlay_obj) adds an overlay element"""
+	add_overlay(self, overlay_obj, overwrite=False) adds an overlay element
+	hide(self, elements=None) hides elements"""
 
 	def __init__(self):
 		self.objects = {}
 		self.objs_on_screen = set()
 
-	def add_overlay_element(self, overlay_obj):
+	def add_overlay_element(self, overlay_obj, overwrite=False):
 		"""Adds an overlay element to the whole overlay.
 
-		This method requires an instance.
-		It is called as follows: instance.add_overlay_element(overlay_obj)
-		For custom objects see class overlay_object.
+		overwrite determines that if an object with the name already
+		exists if it should be overwritten
+		whereas the object must have following vars:
+			name as string
+			active as bool
+		and following funtions:
+			activate() sets active to True. May include animation
+			deactivate() sets active to False. May include animation
+			hide() sets active to False. Happens immediately
+			unhide() sets active to True. Happens immediately
+			blit(screen) blits its image onto the screen variable
 		"""
-		self.objects[overlay_obj.name] = overlay_obj
 
-	def enable(self, name_list):
-		"""Enable the overlay."""
-		tmp_list = []
-		if name_list == "all":
-			tmp_list = [self.objects[elem] for elem in self.objects.keys()]
-		else:
-			if type(name_list) == str:
-				name_list = [name_list]
-			for obj in self.objects:
-				if obj in name_list:
-					tmp_list.append(self.objects[obj])
-		self.objs_on_screen.update(tmp_list)
+		if (overlay_obj.name not in self.objects) or overwrite:
+			overlay_obj.deactivate()
+			self.objects[overlay_obj.name] = overlay_obj
 
-	def disable(self, name_list):
-		"""Disable the overlay."""
-		if name_list == "all":
-			self.objs_on_screen = set()
+	def activate(self, elements=None):
+		"""Unhides elements.
+		When called with no arguments, all elements get shown.
+		Can also be called with name or a list with names.
+		"""
+
+		if elements is None:
+			for object_name in self.objects:
+				self.objects[object_name].activate()
 		else:
-			if type(name_list) == str:
-				name_list = name_list.split(",")
-			for obj in self.objects:
-				if obj in name_list:
-					self.objs_on_screen.remove(self.objects[obj])
-					return
+			if type(elements) != list:
+				elements = list(elements)
+			for element_name in elements:
+				self.objects[element_name].activate()
+
+	def hide(self, elements=None):
+		"""Hides elements.
+		When called with no arguments, all elements get hidden.
+		Can also be called with name or a list with names.
+		"""
+
+		if elements is None:
+			for object_name in self.objects:
+				self.objects[object_name].deactivate()
+		else:
+			if type(elements) != list:
+				elements = list(elements)
+			for element_name in elements:
+				self.objects[element_name].deactivate()
 
 	def blit(self, screen):
-		for obj in self.objects:
-			pass
+		for object_name in self.objects:
+			self.objects[object_name].blit(screen)
 
 
-class create_overlay_object():
+class overlay_element_base_class():
 	"""This is an element which can be used in an overlay"""
 
-	def __init__(self, name):
+	def __init__(self, name, pos, alignment="topleft"):
 		self.name = name
-		self.img = pygame.Surface((0, 0))
-		self.pos = pygame.Rect(0, 0, 0, 0)
+		self.pos = pygame.Rect((0, 0, 0, 0))
+		#TODO implement size
+		self.set_alignment(pos, "topleft")
+		self.active = False
+
+	def set_alignment(self, pos, alignment):
+		if not alignment in ["topleft", "topright", "bottomleft", "botomright",
+				"center", "midtop", "midleft", "midright", "midbottom"]:
+					raise TypeError("Alignment is of unknown type: " + str(alignment))
+		if alignment == "topleft":
+			self.pos.topleft = pos
+		elif alignment == "topright":
+			self.pos.topright = pos
+		elif alignment == "bottomleft":
+			self.pos.bottomleft = pos
+		elif alignment == "bottomright":
+			self.pos.bottomright = pos
+		elif alignment == "center":
+			self.pos.center = pos
+		elif alignment == "midtop":
+			self.pos.midtop = pos
+		elif alignment == "midleft":
+			self.pos.midleft = pos
+		elif alignment == "midright":
+			self.pos.midright = pos
+		elif alignment == "midbottom":
+			self.pos.midbottom = pos
+		else:
+			raise Exception("I have no idea what you have done to crash this…")
+
+	def activate(self):
+		self.active = True
+
+	def deactivate(self):
+		self.active = False
--- src/tests/test_overlay_handler.py
+++ src/tests/test_overlay_handler.py
@@ -9,28 +9,30 @@ class TestOverlay(unittest.TestCase):
 
 	def setUp(self):
 		self.test_obj = overlay_handler.create_overlay()
-		self.overlay_element = overlay_handler.create_overlay_object("test")
-		self.overlay_element2 = overlay_handler.create_overlay_object("test2")
+		self.overlay_element = (
+				overlay_handler.overlay_element_base_class("test", (0, 0)))
+		self.overlay_element2 = (
+					overlay_handler.overlay_element_base_class("test2", (0, 0)))
 		self.test_obj.add_overlay_element(self.overlay_element)
 		self.test_obj.add_overlay_element(self.overlay_element2)
+
+	def test_alignment(self):
+		self.assertEqual(self.overlay_element.pos.topleft, (0, 0))
 
 	def test_elems(self):
 		self.assertTrue(hasattr(self.test_obj, "objects"))
 		self.assertIn("test", self.test_obj.objects)
 		self.assertIsInstance(self.test_obj.objects["test"],
-								overlay_handler.create_overlay_object)
+				overlay_handler.overlay_element_base_class)
 
 	def test_generic(self):
 		self.assertEqual(len(self.test_obj.objects), 2)
-		self.test_obj.enable("all")
-		self.assertEqual(len(self.test_obj.objs_on_screen), 2)
-		self.test_obj.disable("all")
-		self.assertEqual(len(self.test_obj.objs_on_screen), 0)
-		self.test_obj.enable("test")
-		self.assertEqual(len(self.test_obj.objs_on_screen), 1)
-		self.test_obj.disable("test")
-		self.assertEqual(len(self.test_obj.objs_on_screen), 0)
-		self.assertEqual(len(self.test_obj.objects), 2)
+		self.test_obj.activate()
+		for object_name in self.test_obj.objects:
+			self.assertTrue(self.test_obj.objects[object_name].active)
+		self.test_obj.hide()
+		for object_name in self.test_obj.objects:
+			self.assertFalse(self.test_obj.objects[object_name].active)
 
 if __name__ == '__main__':
 	unittest.main()
